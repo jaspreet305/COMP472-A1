44de2ddf4c9b197c825f500e8ee96e6bcf5bc773
Jaspreet Singh <66967130+jaspreet305@users.noreply.github.com>
2023-11-01 14:16:36 -0400

    fix and plot

--- experiment.py
+++ experiment.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def convert_to_one_hot(df, categorical_columns):
     dummies_df = pd.get_dummies(df, columns=categorical_columns, dtype=int)
@@ -14,11 +15,14 @@ penguins_df = pd.read_csv('penguins.csv')
 
 categorical_columns = ['island', 'sex']  
 
-penguins_df_encoded = convert_to_one_hot(penguins_df, categorical_columns)
+penguins_df_1h = convert_to_one_hot(penguins_df, categorical_columns)
+penguins_df_cat = convert_to_categorical_codes(penguins_df, categorical_columns)
 
-penguins_df_encoded = convert_to_categorical_codes(penguins_df, categorical_columns)
+penguins_df_1h.to_csv('transformed_penguins_1h.csv', index=False)
+penguins_df_cat.to_csv('transformed_penguins_categorical.csv', index=False)
 
-
-
-penguins_df_encoded.to_csv('transformed_penguins_1h.csv', index=False)
-penguins_df_encoded.to_csv('transformed_penguins_categorical.csv', index=False)
+species_counts = penguins_df_1h['species'].value_counts(normalize=True) * 100
+species_counts.plot(kind='bar', color=['orange', 'blue', 'limegreen'])
+plt.title('Distribution of Penguin Species')
+plt.ylabel('Percentage')
+plt.xlabel('Species')
